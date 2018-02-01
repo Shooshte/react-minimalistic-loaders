fdbf71b3481a56fe2299003d27662f5a0d0b4723
Miha Sustersic <(none)>
2018-02-01 22:01:47 +0100

    updated the NotchedCircle, JumpyRotation

--- src/animations/JumpyRotation/index.js
+++ src/animations/JumpyRotation/index.js
@@ -5,10 +5,10 @@ export const JumpyRotation = keyframes`
     transform: rotate(0deg) scale(0, 0);
   }
   30% {
-    transform: rotate(90deg) scale(3, 3);
+    transform: rotate(90deg) scale(2, 2);
   }
   70% {
-    transform: rotate(180deg) scale(1.5, 1.5);
+    transform: rotate(180deg) scale(1.25, 1.25);
   }
   100% {
     transform: rotate(360deg) scale(0, 0);
--- src/shapes/NotchedCircle/index.js
+++ src/shapes/NotchedCircle/index.js
@@ -2,8 +2,20 @@ import { extend } from "styled-components";
 import { Base } from "../Base/";
 
 export const NotchedCircle = Base.extend`
-  border: 5px solid ${props => props.color};
+  border: calc(${props => props.size} * 0.1) solid ${props => props.color};
   border-radius: 50%;
   border-right-color: transparent;
   background-color: transparent;
+  :before {
+    content: "";
+    display: block;
+    border: calc(${props => props.size} * 0.1) solid ${props => props.color};
+    width: ${props => props.size};
+    height: ${props => props.size};
+    background-color: transparent;
+    border-right-color: transparent;
+    border-radius: 50%;
+    margin: calc(-${props => props.size} * 0.1);
+    transform: rotate(75deg);
+  }
 `;
